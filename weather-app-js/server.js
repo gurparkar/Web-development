5740e7419f0c02856090baf38fb9ddb1021c8812
GURPARKAR SINGH <gurparkarran12@gmail.com>
2020-01-12 15:52:35 +0530

    Destructuring and object shorthand properties applied

--- weather-app-js/server.js
+++ weather-app-js/server.js
@@ -1,16 +1,30 @@
 const geocode = require("./utils/geocode.js")
 const forecast = require("./utils/forecast.js")
 
-geocode("Chochna Gurdaspur", (error,response) => {
-       console.log("Error",error)
-       console.log("Response",response)
+const address = process.argv[2]
+if (!address){
+       console.log("Please provide an address")
+}else {
+       geocode(address, (error,{latitude, longitude, place}) => { // response(object) has a three properties inside it so we will use destructuring here
+              if (error){
+                     return console.log(error)
+              }
+              forecast(latitude,longitude,(error, forecastResponse) =>{
+                     if(error){
+                            return console.log(error)
+                     }
+                     return console.log("In " + place + " weather is " + forecastResponse)
+              })
+              
+       
+              
+       
+       })
+       
 
-})
 
-forecast(37.8267,-122.4233,(error, response) =>{
-       console.log("Error: ", error)
-       console.log("Response: ", response)
-})
+}
+
 
 
 
--- weather-app-js/utils/geocode.js
+++ weather-app-js/utils/geocode.js
@@ -2,18 +2,18 @@ const request = require('request')
 
 const geocode = (address, callback) => {
 
-    const url1 = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + encodeURIComponent(address) + ".json?access_token=pk.eyJ1IjoiZ3VycGFya2FyIiwiYSI6ImNrNTlkNXAyNDBvZDUzZW8xamt3ZHB5YjUifQ.WmS4Aok1P0j5xE1uJC5ukg&limit=1"
+    const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + encodeURIComponent(address) + ".json?access_token=pk.eyJ1IjoiZ3VycGFya2FyIiwiYSI6ImNrNTlkNXAyNDBvZDUzZW8xamt3ZHB5YjUifQ.WmS4Aok1P0j5xE1uJC5ukg&limit=1"
 
-    request({url : url1, json: true},(error, response) =>{
+    request({url, json: true},(error, {body}) =>{ // We have used {body} instead of response.body for shortening our code
            if(error){
                   callback(error, undefined)
-           }else if(response.body.features.length == 0){
+           }else if(body.features.length == 0){
                   callback("Unable to find the location. Try another search",undefined)
            }else{
                   callback(undefined,{
-                         latitude: response.body.features[0].center[1],
-                         longitude: response.body.features[0].center[0],
-                         place: response.body.features[0].place_name
+                         latitude: body.features[0].center[1],
+                         longitude:body.features[0].center[0],
+                         place:body.features[0].place_name
                   })
            }
 })
